e188cf196375bd10c24c737f5ce746ae45e70b29
Lordmams <mamadn21@gmail.com>
2018-12-02 15:36:13 +0100

    adding rect interaction

--- src/ViWork.DB/Scripts/20181022150900.0004.Install-tOwnGroup.sql
+++ src/ViWork.DB/Scripts/20181022150900.0004.Install-tOwnGroup.sql
@@ -11,7 +11,5 @@ create table viw.tOwnGroup
 );
 
 GO
-
-create unique index IX_tOwnGroup_UserId on viw.tOwnGroup(UserId) where UserId <> 0;
-create unique index IX_tOwnGroup_GroupId on viw.tOwnGroup(GroupId) where GroupId <> 0;
+			
 insert into viw.tOwnGroup(UserId, GroupId) values(0,0);
--- src/ViWork.Spa/src/components/PIXI/PixiTest.vue
+++ src/ViWork.Spa/src/components/PIXI/PixiTest.vue
@@ -1,48 +1,148 @@
 <template>
-  <div id="canva">
-  
-    <pixi-renderer>
-     <container>
-        <sprite :x="0" :y="0"  imagePath='../../assets/BLAC.png'/>
-        
-     </container> 
-   
-    </pixi-renderer>
+  <div class="Canva">
+    <button v-on:click="Screen">Demarrer</button>
+    <!-- <img src= "../../assets/BLAC.png"> -->
+     
   </div>
 </template>
-
+ 
 <script>
 
-import PixiRenderer from '../PIXI/PixiRender.vue'
-import Sprite from '../PIXI/PixiSprite.vue'
-import Container from '../PIXI/PixiContainer.vue'
+import Vue  from 'vue'
+import * as PIXI from 'pixi.js'
+import {Sprite} from 'pixi.js'
+import {Renderer} from 'pixi.js'
+//import './app.js'
+//import Render from './PixiRender.vue'
 
 export default {
-  components: {
-    PixiRenderer,
-    Sprite,
-    Container
+  mounted(){
   },
+methods: {
+Screen(){
+  var renderer = PIXI.autoDetectRenderer(1080,800, { backgroundColor: 0x000000, antialias: true });
+document.body.appendChild(renderer.view);
 
-  methods: {
-    scaleObject(container) {
-      container.scale.x *= 1.25;
-      container.scale.y *= 1.25;
+// Create the main stage for your display objects
+var stage = new PIXI.Container();
+
+// Initialize the pixi Graphics class
+var graphics = new PIXI.Graphics();
+
+// Set a new fill color
+graphics.beginFill(0x3498db); // Blue
+
+
+
+// Draw a rectangle
+graphics.drawRect(10, 10, 75, 75); // drawRect(x, y, width, height)
+graphics.endFill();
+graphics.interactive = true;
+graphics.buttonMode = true;
+
+// Add a hit area..
+graphics
+          .on('click', onCreateRect)
+
+        
+
+
+//         // Use mouse-only events
+//         // .on('mousedown', onButtonDown)
+//         // .on('mouseup', onButtonUp)
+//         // .on('mouseupoutside', onButtonUp)
+//         // .on('mouseover', onButtonOver)
+//         // .on('mouseout', onButtonOut)
+
+//         // Use touch-only events
+//         // .on('touchstart', onButtonDown)
+//         // .on('touchend', onButtonUp)
+//         // .on('touchendoutside', onButtonUp)
       
-    },
+  
+ 
 
-    tickInfo(container, delta) {
-      console.log(this.container)
-    }
-  }
+
+
+
+// Add the graphics to the stage
+stage.addChild(graphics);
+
+var superSquare = PIXI.Texture.fromImage('../../assets/BLAC.png');
+
+	superSquare.x = 75;
+  superSquare.y = 50;
+
+
+// Start animating
+animate();
+function animate() {
+    //Render the stage
+    renderer.render(stage);
+    
+    requestAnimationFrame(animate);
 }
+
+function onCreateRect(){
+    var rect = new PIXI.Graphics();
+    rect.beginFill(0x3498db); // Blue
+
+
+
+    // Draw rectangle
+    rect.drawRect(240, 10, 75, 75); // drawRect(x, y, width, height)
+    rect.endFill();
+    rect.interactive = true;
+    rect.buttonMode = true;
+    rect
+        // Mouse & touch 
+        // drag and drop events
+
+        .on('mousedown', onDragStart)
+		    .on('touchstart', onDragStart)
+		    .on('mouseup', onDragEnd)
+		    .on('mouseupoutside', onDragEnd)
+		    .on('touchend', onDragEnd)
+		    .on('touchendoutside', onDragEnd)
+		    .on('mousemove', onDragMove)
+		    .on('touchmove', onDragMove);
+    stage.addChild(rect);
+}
+
+function onDragStart(event) {
+    // store a reference to the data
+    	this.data = event.data;
+	    this.alpha = 0.5;
+	    this.dragging = true;
+	    this.dragPoint = event.data.getLocalPosition(this.parent);
+	    this.dragPoint.x -= this.position.x;
+	    this.dragPoint.y -= this.position.y;
+}
+
+function onDragEnd() {
+    this.alpha = 1;
+    this.dragging = false;
+    // set the interaction data to null
+    this.data = null;
+}
+
+function onDragMove() {
+    if (this.dragging) {
+        var newPosition = this.data.getLocalPosition(this.parent);
+        this.x = newPosition.x;
+        this.y = newPosition.y;
+    }
+}
+ 
+}
+ }
+}
+
 </script>
 
-<style>
-#screen {
-  position: absolute;
-  top: 50%;
-  right: 50%;
-  
+<style lang="scss">
+  canvas {
+  width: 100%;
+  height:100%;
 }
 </style>
