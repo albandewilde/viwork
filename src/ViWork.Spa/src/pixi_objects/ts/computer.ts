0fd001d97bcfbe0b392a1bc96563170b5c6fbf21
Alban <dewilde@intechinfo.fr>
2018-12-14 14:24:00 +0100

    for mams

--- src/ViWork.Spa/src/pixi_objects/ts/computer.ts
+++ src/ViWork.Spa/src/pixi_objects/ts/computer.ts
@@ -1,15 +1,39 @@
-//import {all} from "pixi.js"
+import {PIXI} from "pixi.js"
 
+import {Idrawable} from "./Idrawable"
 import {Computer} from "../../objects/js/computer"
 
-class pixi_Computer {
-    computer: Computer
-    position: [number, number]
+class pixi_Computer implements Idrawable {
+    material: Computer
+    container_position: [number, number]
     sprite_path: string
+    container: PIXI.container
+    sprite: PIXI.sprite
 
     constructor() {
-        this.computer = new Computer()
-        this.position = [0, 0]// sous la rouris
+        this.material = new Computer()
+        this.container_position = PIXI.renderer.interaction.mouse.global    // position du pointeur de la souris
         this.sprite_path = "../../images/sprites/computer.png"
+        this.container = new PIXI.contenair()
+        this.sprite = new PIXI.sprite.fromImage(this.sprite_path)
+        this.sprite.interactive = true
+    }
+
+    take() {
+        // the computer change his sprite and his position is under the pointer while he don't put ip
+    }
+
+    put() {
+        // put the computer at his new position (fixed position), don't forget to change his sprite
+    }
+
+    draw() {
+        this.container.addChild(this.sprite)
+    }
+
+    remove() {
+    }
+
+    async animation(){
     }
 }
