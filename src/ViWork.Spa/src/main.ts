01a3e437798fd2993f1fdc2ae10e95b7fda8b16f
Baptiste TEXIER <bapt.tex@gmail.com>
2018-12-17 10:32:45 +0100

    fixing some problem

--- src/ViWork.Spa/src/main.ts
+++ src/ViWork.Spa/src/main.ts
@@ -1,11 +1,12 @@
 import Vue from "vue";
-import App from "./App.vue";
+import App from "./components/App.vue";
 import AuthService from "./services/AuthService";
-import ElementUI from "element-ui";
-import router from "./router";
+import router from "./main.router";
 import store from "./store";
 import "./main.vendors";
 import "./plugins/element.js";
+import './plugins/element.js';
+import ElementUI from "element-ui"
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
