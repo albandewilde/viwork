d62be1546d929eb743d4cddb170b6987a11f58ef
Alban <dewilde@intechinfo.fr>
2018-12-17 14:19:39 +0100

    Write a lof of code, considere the description on this commit
    write the router function and the arp and tcp protocole
    /!\ it wasn't tested for now
    
    - all things bollow are tested and functionals
    
    you can configre your network card
    that mean you can choose on which wire in the cable the machine
    write and listen information
    
    I also add the broadcast address on the layer two

--- src/ViWork.Spa/src/objects/ts/computer.ts
+++ src/ViWork.Spa/src/objects/ts/computer.ts
@@ -1,7 +1,8 @@
 import {Roucom} from "./roucom"
 
 export class Computer extends Roucom{
-    constructor(nb_network_card: number=1) {
-        super(nb_network_card)
+    // network_cards of computer write paquet in the element 1 of a cable and listen in the element 0
+    constructor(nb_network_card: number=1, route_eh: boolean=false, write_on_cable: number=1) {
+        super(nb_network_card, route_eh, write_on_cable)
     }
 }
