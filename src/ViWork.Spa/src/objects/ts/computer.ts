23ff8c9a215aa7fad8f0c24c09626310f7d55740
Lordmams <mamadn21@gmail.com>
2018-12-17 10:33:10 +0100

    Take it Jasmine

--- src/ViWork.Spa/src/objects/ts/computer.ts
+++ src/ViWork.Spa/src/objects/ts/computer.ts
@@ -1,6 +1,6 @@
-import {Roucom} from "./roucom"
+import {Roucom} from "./roucom";
 
-export default class Computer extends Roucom{
+export class Computer extends Roucom{
     constructor(nb_network_card: number=1) {
         super(nb_network_card)
     }
